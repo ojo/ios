950f48c58f27b583d06054a588922a74977734c7
Boris Suvorov <bsuvorov@gmail.com>
2016-12-26 17:54:25 -0800

    Create NewsItemViewController

---
+++ ojo/NewsItemViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewsItemViewController.swift
+//  ojo
+//
+//  Created by Boris Suvorov on 12/26/16.
+//  Copyright © 2016 TTRN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsItemViewController : UIViewController {
+    var newsItem: NewsItem?
+    
+    init(newsItem: NewsItem) {
+        self.newsItem = newsItem
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
