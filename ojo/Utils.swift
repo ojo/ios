93770e5aaee704c6a366ced4666213aadc95437c
BTC <brian.holderchow@gmail.com>
2016-12-27 11:35:20 -0800

    new constants

--- ojo/Utils.swift
+++ ojo/Utils.swift
@@ -9,6 +9,11 @@
 import Foundation
 import UIKit
 
+// you can use http://uicolor.xyz/#/hex-to-ui to convert hex to UIColor!
+
 let DEFAULT_VC_BACKGROUND_COLOR = UIColor.white
 let DEFAULT_RED_COLOR = UIColor(red:0.85, green:0.00, blue:0.00, alpha:1.0)
+let DEFAULT_MARGIN_PX = CGFloat(6)
+let DEFAULT_PLACEHOLDER_TEXT = "REPLACE ME"
+let DEFAULT_FONT = "HelveticaNeue"
 let DEFAULT_FONT_BOLD = "HelveticaNeue-Bold"
