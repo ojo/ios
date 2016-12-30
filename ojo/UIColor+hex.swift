3fa1f359e39bc8d3332c3960cd5c094097072f80
BTC <brian.holderchow@gmail.com>
2016-12-30 14:29:52 -0800

    add UIColor hex converter

---
+++ ojo/UIColor+hex.swift
@@ -0,0 +1,24 @@
+//
+//  UIColor+hex.swift
+//  ora2
+//
+//  Created by btc on 2/18/16.
+//  Copyright © 2016 btc. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    /**
+     The six-digit hexadecimal representation of color of the form #RRGGBB.
+     
+     - parameter hex6: Six-digit hexadecimal value.
+     */
+    public convenience init(hex6: UInt32, alpha: CGFloat = 1) {
+        let divisor = CGFloat(255)
+        let red     = CGFloat((hex6 & 0xFF0000) >> 16) / divisor
+        let green   = CGFloat((hex6 & 0x00FF00) >>  8) / divisor
+        let blue    = CGFloat( hex6 & 0x0000FF       ) / divisor
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
