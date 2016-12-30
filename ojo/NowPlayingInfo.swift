2a38b43df8b6a1d117d99650d3f8cb7fc68a976b
BTC <brian.holderchow@gmail.com>
2016-12-30 11:41:15 -0800

    create NowPlayingInfo struct

---
+++ ojo/NowPlayingInfo.swift
@@ -0,0 +1,16 @@
+//
+//  NowPlayingInfo.swift
+//  ojo
+//
+//  Created by Brian Tiger Chow on 12/29/16.
+//  Copyright © 2016 TTRN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct NowPlayingInfo {
+    let title: String
+    let artist: String
+    let image: UIImage
+}
