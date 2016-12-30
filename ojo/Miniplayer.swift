b2a58b217f15c2622926ca0577c816bc21750a5c
BTC <brian.holderchow@gmail.com>
2016-12-30 14:23:28 -0800

    add miniplayer

---
+++ ojo/Miniplayer.swift
@@ -0,0 +1,43 @@
+//
+//  Miniplayer.swift
+//  ojo
+//
+//  Created by Brian Tiger Chow on 12/29/16.
+//  Copyright © 2016 TTRN. All rights reserved.
+//
+
+import UIKit
+
+class Miniplayer: PlaybackDelegate {
+    
+    private var playbackManager: PlaybackManager
+    
+    private var root: UINavigationController?
+
+    init(_ navigationController: UINavigationController,
+         playbackManager: PlaybackManager) {
+        root = navigationController
+        self.playbackManager = playbackManager
+        self.playbackManager.addDelegate(self)
+    }
+    
+    func playbackDidChangeState(_ s: PlaybackState) {
+        switch s {
+        case .buffering: break
+        case .started: break
+        case .stopped: break
+        }
+    }
+
+    func showMiniplayer() {
+
+        let vc = NowPlayingViewController(playbackManager: playbackManager)
+        root?.presentPopupBar(withContentViewController: vc,
+                              animated: true,
+                              completion: nil)
+    }
+    
+    func hideMiniplayer() {
+        root?.dismissPopupBar(animated: true, completion: nil)
+    }
+}
