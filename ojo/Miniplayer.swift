6817cbb26c4ac55dc2f0833ed7b9d70b9f3b4bdb
BTC <brian.holderchow@gmail.com>
2016-12-31 13:17:24 -0800

    feat(Miniplayer): display title, artist

--- ojo/Miniplayer.swift
+++ ojo/Miniplayer.swift
@@ -13,31 +13,49 @@ class Miniplayer: PlaybackDelegate {
     private var playbackManager: PlaybackManager
     
     private var root: UINavigationController?
+    
+    private var nowPlaying: NowPlayingViewController
 
     init(_ navigationController: UINavigationController,
          playbackManager: PlaybackManager) {
-        root = navigationController
+        self.root = navigationController
         self.playbackManager = playbackManager
+        self.nowPlaying = NowPlayingViewController(playbackManager: playbackManager)
         self.playbackManager.addDelegate(self)
     }
     
     func didChange(state: PlaybackState) {
         switch state {
         case .buffering:
+            // TODO(btc): update playback toggle to reflect buffering state
             showMiniplayer()
         case .started:
+            // TODO(btc): update playback toggle to pause/stop button
             showMiniplayer()
         case .stopped: break
         }
     }
     
     func incoming(info: NowPlayingInfo) {
-        // TODO(btc): Impl
+        
+        if info.title == "" {
+            // then the server hasn't been fixed yet.
+            nowPlaying.popupItem.title = info.artist
+            nowPlaying.popupItem.subtitle = info.album
+        } else {
+            nowPlaying.popupItem.title = info.title
+            nowPlaying.popupItem.subtitle = info.artist
+        }
+        
+        // replace station image with info.artwork.image when it loads async'ly
+        // change the contentMode of the imageView so that the station image
+        // isn't distorted
+        nowPlaying.popupItem.image = playbackManager.station?.image
+        // TODO(btc): nowPlaying?.popupItem.image = future.result
     }
 
     func showMiniplayer() {
-        let vc = NowPlayingViewController(playbackManager: playbackManager)
-        root?.presentPopupBar(withContentViewController: vc,
+        root?.presentPopupBar(withContentViewController: nowPlaying,
                               animated: true,
                               completion: nil)
     }
