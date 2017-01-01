28e48a952600623c3b4ad493edee47c360dccb27
BTC <brian.holderchow@gmail.com>
2017-01-01 07:15:38 -0800

    misc: cleanup

--- ojo/AppDelegate.swift
+++ ojo/AppDelegate.swift
@@ -16,15 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        
         let vc = StationsViewControllerFactory.make(withStations: STATIONS,
                                                      bounds: UIScreen.main.bounds)
         let navigationController = RootNavigationController(rootViewController: vc)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
-        // Override point for customization after application launch.
         return true
     }
 
