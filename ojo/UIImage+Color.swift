6924fe728dee135a6eb0acaca3de5e7c3ee98fa0
BTC <brian.holderchow@gmail.com>
2017-01-02 16:32:32 -0800

    refactor

--- ojo/UIImage+Color.swift
+++ ojo/UIImage+Color.swift
@@ -10,15 +10,14 @@ import UIKit
 
 extension UIImage {
     static func from(color: UIColor, withSize size: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContext(size)
+        defer { UIGraphicsEndImageContext() }
         let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
         color.setFill()
         UIRectFill(rect)
         if let image: UIImage = UIGraphicsGetImageFromCurrentImageContext() {
-            UIGraphicsEndImageContext()
             return image
         }
-        UIGraphicsEndImageContext()
         return nil
     }
 }
